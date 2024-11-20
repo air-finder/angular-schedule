fb06f09fef68becf7f2ae02e907c3e9450b04e09
Bruno Silva <brunovinicius0704@gmail.com>
2024-11-20 17:47:05 -0300

    feat: ajustes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  scheduleUrl: 'https://localhost:5000',
-  authUrl: 'http://localhost:50001',
-  // paymentsUrl: 'https://dev.payments-api.bvsilva.com',
+  scheduleUrl: 'https://schedule-api.bvsilva.com',
+  authUrl: 'https://auth-api.bvsilva.com',
 }
