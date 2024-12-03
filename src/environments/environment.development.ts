1d001874359d5d7441a0c6f9ca2617c1a83803e6
Bruno Silva <brunovinicius0704@gmail.com>
2024-12-02 23:27:54 -0300

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   scheduleUrl: 'https://schedule-api.bvsilva.com',
-  authUrl: 'https://suth-api.bvsilva.com',
+  authUrl: 'https://auth-api.bvsilva.com',
 }
